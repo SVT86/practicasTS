f3bdbc96800faeead75eb99b4487fa363c7cd45b
Seba <sebatl2070@gmail.com>
2021-05-16 20:36:34 -0300

    practicas en TS -version 1.0

---
+++ main.ts
@@ -0,0 +1,109 @@
+class Persona {
+    private nombre: string;
+    private apellido: string;
+    private direccion: any;
+
+    public constructor(nombre: string, apellido: string, direccion: any) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public getPersona(): string {
+        return this.nombre + " " + this.apellido;
+    }
+
+    public getNombre(): string {
+        return this.nombre;
+    }
+
+    public getApellido(): string {
+        return this.apellido;
+    }
+
+    public getDireccion():any {
+        return this.direccion;
+    }
+}
+
+class Direccion {
+    public constructor(private calle: string, private altura: number) { };
+
+    public getCalle(): string {
+        return this.calle;
+    }
+
+    public getAltura(): number {
+        return this.altura;
+    }
+}
+
+
+class Cliente extends Persona {
+
+    private id: number;
+    constructor(nombre: string, apellido: string, identificador: number, direccion?: any)  {
+        super(nombre, apellido, direccion);
+        this.id = identificador;
+    }
+
+    public getCliente(): number {
+
+        return this.id;
+    }
+
+    public verAutomovil(elAuto: Automovil): any {
+
+        return {
+            marca: elAuto.getMarca(),
+            modelo: elAuto.getModelo()
+        }
+    }
+}
+
+class Automovil {
+
+
+    public constructor(private marca: string, private modelo: string) {
+
+    }
+
+    public getAutomovil(): string {
+        return this.marca + " " + this.modelo;
+    }
+
+    public getMarca(): string {
+        return this.marca
+    }
+
+    public getModelo(): string {
+        return this.modelo;
+    }
+
+}
+
+let direccionSebastian = new Direccion("Melo", 141);
+let direccionPedro = new Direccion("Belgrano", 590);
+let direccionClienteJuan = new Direccion("Sarmiento",640);
+
+let sebastian = new Persona("Sebastian", "Levi", direccionSebastian);
+let pedro = new Persona("Pedro", "Ramos", direccionPedro);
+
+
+let clienteJuan = new Cliente("Juan", "Ramirez",45556,direccionClienteJuan);// en este caso direccion puede se vacio - ver def de clase-
+
+let honda = new Automovil("Honda", "Accord");
+
+clienteJuan.verAutomovil(honda);
+
+console.log(sebastian.getPersona()," y vive en ",sebastian.getDireccion().calle," al ",sebastian.getDireccion().altura);
+console.log(pedro.getPersona()," y vive en la calle ",pedro.getDireccion().calle," altura ",pedro.getDireccion().altura);
+
+console.log("El cliente ", clienteJuan.getPersona(),"con un ID: ",clienteJuan.getCliente(), ", usa un ", clienteJuan.verAutomovil(honda).marca, " modelo ", clienteJuan.verAutomovil(honda).modelo, ".-");
+console.log("y vive en ",clienteJuan.getDireccion().calle," al ",clienteJuan.getDireccion().altura);
+
+
+
+//***************************************************** */
+
+console.log("Persona: ", clienteJuan.getPersona());
